4d4da7abdfb0734ca6e0b4edcbec25e579357311
zsebas7 <scaparroz724@alumnos.iua.edu.ar>
2026-06-10 15:47:50 -0300

    fix: corregir tipos Long/String en pantallas y import StatsMode

--- app/src/main/java/com/undef/superahorro/caparrozruiz/data/model/Purchase.kt
+++ app/src/main/java/com/undef/superahorro/caparrozruiz/data/model/Purchase.kt
@@ -1,7 +1,7 @@
 package com.undef.superahorro.caparrozruiz.data.model
 
 data class Purchase(
-    val id: Long,
+    val id: Long = 0,
     val market: String,
     val date: String,
     val time: String,
